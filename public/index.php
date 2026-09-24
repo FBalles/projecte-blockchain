e8eef03856e4058f49e43959111ddc6e8b82ee40
ferranPC <fballes7>
2026-09-24 21:13:03 +0200

    test2

--- public/index.php
+++ public/index.php
@@ -1,10 +1,9 @@
 <?php
-// Put this at the very top of public/index.php
+declare(strict_types=1);
+
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-
-declare(strict_types=1);
 
 require_once __DIR__ . '/../src/Config.php';
 require_once __DIR__ . '/../src/Auth.php';
